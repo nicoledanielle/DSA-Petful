1f493e2ade624097a391482b7467849212966dfb
Nicole Gilbert <nicolegilbert16@gmail.com>
2018-02-02 12:13:08 -0800

    Pet component is rendering correctly

--- src/Dashboard.js
+++ src/Dashboard.js
@@ -1,52 +1,16 @@
 import React, {Component} from 'react';
+import Pet from './Pet';
 
 export class Dashboard extends Component {
+  componentDidMount(){
+    console.log('here');
+  }
+
   render() {
     return (
       <div>
-        <section className="adoptable-cat">
-            <header>
-              <h1>Fluffy</h1>
-              <img src="https://assets3.thrillist.com/v1/image/2622128/size/tmg-slideshow_l.jpg" />
-            </header>
-            <main>
-              <dl>
-              <dt>Sex</dt>
-              <dd>Female</dd>
-              <dt>Age</dt>
-              <dd>2</dd>
-              <dt>Breed</dt>
-              <dd>Bengal</dd>
-              <dt>Story</dt>
-              <dd>Thrown on the street</dd>
-              </dl>
-              <button>
-                Adopt!
-              </button>
-            </main>
-        </section>
-
-        <section className="adoptable-dog">
-            <header>
-              <h1>Zeus</h1>
-              <img src="http://www.dogster.com/wp-content/uploads/2015/05/Cute%20dog%20listening%20to%20music%201_1.jpg" />
-            </header>
-            <main>
-              <dl>
-              <dt>Sex</dt>
-              <dd>Male</dd>
-              <dt>Age</dt>
-              <dd>3</dd>
-              <dt>Breed</dt>
-              <dd>Golden Retriever</dd>
-              <dt>Story</dt>
-              <dd>Owner passed away</dd>
-              </dl>
-              <button>
-                Adopt!
-              </button>
-            </main>
-        </section>
+          <Pet adoptablePet={this.props.adoptableCat} />
+          <Pet adoptablePet={this.props.adoptableDog} />
       </div>
     )
   }
---
+++ src/Pet.js
@@ -0,0 +1,34 @@
+import React from 'react';
+
+export default function Pet(props) {
+	const name = props.adoptablePet.map(pet => pet.name);
+	const url = props.adoptablePet.map(pet => pet.imageURL);
+	const desc = props.adoptablePet.map(pet => pet.imageDescription);
+	const sex = props.adoptablePet.map(pet => pet.sex); 
+	const age = props.adoptablePet.map(pet => pet.age); 
+	const breed = props.adoptablePet.map(pet => pet.breed); 
+	const story = props.adoptablePet.map(pet => pet.story); 
+	return(
+		<section>
+			<header>
+				<h1>{name}</h1>
+				<img src={url} alt={desc}/>
+				</header>
+			<main>
+				<dl>
+				<dt>Sex</dt>
+				<dd>{sex}</dd>
+				<dt>Age</dt>
+				<dd>{age}</dd>
+				<dt>Breed</dt>
+				<dd>{breed}</dd>
+				<dt>Story</dt>
+				<dd>{story}</dd>
+				</dl>
+				<button>
+					Adopt!
+				</button>
+			</main>
+		</section>
+		)
+}
