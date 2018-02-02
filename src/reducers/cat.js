2ab8fb85f48d263a32e4566950792348f11356a7
Nicole Gilbert <nicolegilbert16@gmail.com>
2018-02-02 12:53:11 -0800

    added reducers for dog and cat actions

---
+++ src/reducers/cat.js
@@ -0,0 +1,54 @@
+import{
+	FETCH_CAT_REQUEST,
+	FETCH_CAT_SUCCESS,
+	FETCH_CAT_ERROR,
+	ADOPT_CAT_REQUEST,
+	ADOPT_CAT_SUCCESS,
+	ADOPT_CAT_ERROR
+} from './actions/cat'
+
+const initialState ={
+	cats: [],
+	loading: false,
+	error: null
+}
+
+export const catReducer = (state = initialState, action) => {
+	if(action.type === FETCH_CAT_REQUEST){
+		return Object.assign({}, state, {
+			loading: true,
+			error: null
+		})
+	}
+	else if(action.type===FETCH_CAT_SUCCESS){
+		return Object.assign({}, state, {
+			loading: false,
+			error: null,
+			cats: action.cats
+		})
+	}
+	else if(action.type===FETCH_CAT_ERROR){
+		return Object.assign({}, state, {
+			error: action.error,
+			loading: false
+		})
+	}
+	else if(action.type===DELETE_CAT_REQUEST){
+		return Object.assign({}, state, {
+			loading: false,
+			error: null
+		})
+	}
+	else if(action.type===DELETE_CAT_SUCCESS){
+		return Object.assign({}, state, {
+			cats: action.cats
+		})
+	}
+	else if (action.type === DELETE_CAT_ERROR){
+		return Object.assign({}, state, {
+			error: action.error,
+			loading: false
+		})
+	}
+	return state;
+}
---
+++ src/reducers/dog.js
@@ -0,0 +1,54 @@
+import{
+	FETCH_DOG_REQUEST,
+	FETCH_DOG_SUCCESS,
+	FETCH_DOG_ERROR,
+	ADOPT_DOG_REQUEST,
+	ADOPT_DOG_SUCCESS,
+	ADOPT_DOG_ERROR
+} from './actions/dog'
+
+const initialState ={
+	dogs: [],
+	loading: false,
+	error: null
+}
+
+export const dogReducer = (state = initialState, action) => {
+	if(action.type === FETCH_DOG_REQUEST){
+		return Object.assign({}, state, {
+			loading: true,
+			error: null
+		})
+	}
+	else if(action.type===FETCH_DOG_SUCCESS){
+		return Object.assign({}, state, {
+			loading: false,
+			error: null,
+			dogs: action.dogs
+		})
+	}
+	else if(action.type===FETCH_DOG_ERROR){
+		return Object.assign({}, state, {
+			error: action.error,
+			loading: false
+		})
+	}
+	else if(action.type===DELETE_DOG_REQUEST){
+		return Object.assign({}, state, {
+			loading: false,
+			error: null
+		})
+	}
+	else if(action.type===DELETE_DOG_SUCCESS){
+		return Object.assign({}, state, {
+			dogs: action.dogs
+		})
+	}
+	else if (action.type === DELETE_DOG_ERROR){
+		return Object.assign({}, state, {
+			error: action.error,
+			loading: false
+		})
+	}
+	return state;
+}
